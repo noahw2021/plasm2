341722c2982fd4db828979a08a1592ddbfa65125
noahw2021 <noah.wooten@icloud.com>
2023-04-16 08:04:30 -0400

    implement bitwise

--- plasm2_emu/cpu/ops/cops_bitwise.c
+++ plasm2_emu/cpu/ops/cops_bitwise.c
@@ -7,3 +7,41 @@ plasm2_emu
 (c) Noah Wooten 2023, All Rights Reserved
 */
 
+void __AND(void) {
+	union {
+		byte Input;
+		struct {
+			byte Destination : 4;
+			byte Source : 4;
+		};
+	}Inputs;
+	Inputs.Input = mmu_read1(++i->ip);
+	i->rs_gprs[Inputs.Destination] &= i->rs_gprs[Inputs.Source];
+	return;
+}
+
+void __BOR(void) {
+	union {
+		byte Input;
+		struct {
+			byte Destination : 4;
+			byte Source : 4;
+		};
+	}Inputs;
+	Inputs.Input = mmu_read1(++i->ip);
+	i->rs_gprs[Inputs.Destination] |= i->rs_gprs[Inputs.Source];
+	return;
+}
+
+void __XOR(void) {
+	union {
+		byte Input;
+		struct {
+			byte Destination : 4;
+			byte Source : 4;
+		};
+	}Inputs;
+	Inputs.Input = mmu_read1(++i->ip);
+	i->rs_gprs[Inputs.Destination] ^= i->rs_gprs[Inputs.Source];
+	return;
+}
