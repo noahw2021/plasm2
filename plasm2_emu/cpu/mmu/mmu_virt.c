6153b0f4c89b1a08437606d800c2ba345a7543b9
noahw2021 <noah.wooten@icloud.com>
2023-04-16 07:19:01 -0400

    pending virtual memory

---
+++ plasm2_emu/cpu/mmu/mmu_virt.c
@@ -0,0 +1,19 @@
+#include "../cpu.h"
+#include "mmu.h"
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+/*
+mmu_virt.c
+plasm2
+plasm2_emu
+(c) Noah Wooten 2023, All Rights Reserved
+*/
+
+u64 mmu_translate(u64 VirtualAddress) {
+	if (i->flags_s.VF) {
+		return VirtualAddress;
+	} else {
+		return VirtualAddress;
+	}
+}
