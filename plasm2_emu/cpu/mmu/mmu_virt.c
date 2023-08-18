435d46163a25f8f981a7db252429a75847dec947
noah <noah.wooten@icloud.com>
2023-08-18 10:20:13 -0400

    remove maxaddr function

--- plasm2_emu/cpu/mmu/mmu_virt.c
+++ plasm2_emu/cpu/mmu/mmu_virt.c
@@ -40,25 +40,3 @@ u64 mmu_translate(u64 VirtualAddress, byte Reason, u64 MaxSize) {
 		return VirtualAddress;
 	}
 }
-
-u64 mmu_maxaddr(u64 VirtualAddress, byte Reason) {
-	if (i->flags_s.VF) {
-		for (int i = 0; i < mmuctx->PageCount; i++) {
-			if (InRange(VirtualAddress, mmuctx->Pages[i].Virtual, mmuctx->Pages[i].Virtual + mmuctx->Pages[i].Size)) {
-				if ((Reason & REASON_READ) && !mmuctx->Pages[i].Read)
-					return 0;
-				if ((Reason & REASON_WRTE) && !mmuctx->Pages[i].Write)
-					return 0;
-				if ((Reason & REASON_EXEC) && !mmuctx->Pages[i].Execute)
-					return 0;
-				if (VirtualAddress >= (mmuctx->Pages[i].Virtual + mmuctx->Pages[i].Size))
-					return 0;
-
-				return mmuctx->Pages[i].Virtual + mmuctx->Pages[i].Size - 1;
-			}
-		}
-	} else {
-		return PHYS_MEMSZ;
-	}
-	return 0x00;
-}
