d41ce6f6de1bd314894587723c3cb2d465ce3e6e
Noah Wooten <noah.wooten@icloud.com>
2023-12-28 13:26:52 -0500

    refactor symbols

--- plasm2_asm/link/link.h
+++ plasm2_asm/link/link.h
@@ -14,15 +14,18 @@ WORD64 LinkGetSymbol(const char* Name, WORD32 Offset);
 void LinkResolve(const char* Name, WORD64 Pointer);
 void LinkGo(void);
 
+typedef struct _LINK_CTX_SYMBOL {
+    WORD64 Resolution;
+    char* SymbolName;
+    BYTE Resolved;
+
+    WORD64 LocationCount;
+    WORD64* Locations;
+}LINK_CTX_SYMBOL, *PLINK_CTX_SYMBOL;
+
 typedef struct _LINK_CTX {
 	WORD64 SymbolCount;
-	struct {
-		WORD64 Resolution;
-		char* SymbolName;
-		BYTE Resolved;
-
-		WORD64 LocationCount;
-		WORD64* Locations;
-	}*Symbols;
+    PLINK_CTX_SYMBOL Symbols;
 }LINK_CTX, *PLINK_CTX;
+
 extern PLINK_CTX LinkCtx;
--- plasm2_asm/link/link_go.c
+++ plasm2_asm/link/link_go.c
@@ -16,17 +16,20 @@ void LinkGo(void) {
 	for (int s = 0; s < LinkCtx->SymbolCount; s++) {
 		if (!LinkCtx->Symbols[s].Resolved) {
 			char* ErrorMsg = malloc(512);
-			sprintf(ErrorMsg, "[E5000]: Unresolved symbol '%s'.", LinkCtx->Symbols[s].SymbolName);
+			sprintf(ErrorMsg, "[E5000]: Unresolved symbol '%s'.", 
+                LinkCtx->Symbols[s].SymbolName);
 			CgeError(0xFFFF, ErrorMsg);
 			free(ErrorMsg);
 		}
 
 		for (int l = 0; l < LinkCtx->Symbols[s].LocationCount; l++) {
-			fseek(PrimaryOutput, (WORD32)LinkCtx->Symbols[s].Locations[l], SEEK_SET);
-			// linkctx->Symbols[s].Resolution; // += cgctx->ReferencePtr; WHY??
-			fwrite(&LinkCtx->Symbols[s].Resolution, 8, 1, PrimaryOutput);
+            PLINK_CTX_SYMBOL ThisSymbol = &LinkCtx->Symbols[s];
+            
+            fseek(PrimaryOutput, (WORD32)ThisSymbol->Locations[l], SEEK_SET);
+			fwrite(&ThisSymbol->Resolution, 8, 1, PrimaryOutput);
 		}
 
+        // @TODO (fix this ?? -nw 12/28/23)
 		if (LinkCtx->Symbols[s].Locations)
 			//free(linkctx->Symbols[s].Locations);
 		LinkCtx->Symbols[s].LocationCount = 0;
